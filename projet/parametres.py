89e49df763a993e2419c995f280e0d64a5e17c56
RAVIA <p58564@etu.hepl.ch>
2024-12-09 21:19:24 +0100

    Add Paramètres

---
+++ projet/parametres.py
@@ -0,0 +1,34 @@
+
+
+#Paramètres de départ pour les plantes
+plante_quantite_initial = 700
+plante_energie_initiale = 3
+plante_energie_maximale = 3
+plante_energie_fournie = 3
+plante_age_maximal = "infini"
+plante_valeur_nutritive = 3
+
+
+#Paramètres de départ pour les plantes
+lapin_nombre_initial = 520
+lapin_energie_initiale = 10
+lapin_energie_maximale = 20
+lapin_vitesse = 1
+lapin_age_maximal = 5
+lapin_reproduction_max = 3
+lapin_reproduction_min = 1
+lapin_couts_reproduction = 2 #a définir proportionnel aux nombres d'enfants ?
+lapin_valeur_nutritive = 16
+lapin_cout_deplacement =  4 #A définir
+
+
+#Class Renard, paramètres initiaux :
+renard_nombre_initial = 22
+renard_energie_initiale = 25
+renard_energie_maximale = 50
+renard_vitesse = 2
+renard_age_maximal = 3
+renard_reproduction_max = 5
+renard_reproduction_min = 1
+renard_couts_reproduction = 4 #A definir proportionnel au nombre d'enfants ?
+renard_couts_deplacement = 5 #a definir  proportionnel au nombre de case 
