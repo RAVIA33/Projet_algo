3db39e03cd7e4227521bb787e858a0cb35807357
RAVIA <p58564@etu.hepl.ch>
2024-12-09 21:18:50 +0100

    Add reproduction

--- projet/entities.py
+++ projet/entities.py
@@ -1,16 +1,18 @@
 from typing import Tuple
+from typing import List
+import random
 
 class EtreVivant  : 
     #Attributs communs à tous les êtres vivants
-    energie : int                   #Energie actuelle de l'entité
+    energie: int                   #Energie actuelle de l'entité
     energie_maximale : int          
     position : Tuple[int, int]
     age_maximal : int               # Durée de vie 
     age : int                       #Âge actuel
     vivant : bool                   #Indique si l'être est vivant ou non
     
-    def __init__(self, energie_initiale : int, energie_maximale : int, age_maximale:int, vivant : bool ) :
-        self.energie = energie_initiale
+    def __init__(self,  energie_initiale : int, energie_maximale : int, age_maximale:int, vivant : bool ) :
+        self.energie_actuelle = energie_initiale
         self.energie_maximale = energie_maximale
         self.age = 0
         self.age_maximale = age_maximale
@@ -28,70 +30,71 @@ class EtreVivant  :
         if self.age > self.age_maximal :
             self.vivant = False 
 
-
-#Paramètres de départ pour les plantes
-plante_quantite_initial = 700
-plante_energie_initiale = 3
-plante_energie_fournie = 3
-
 class Plante(EtreVivant) :
-    valeur_nutritive : int
+    valeur_nutritive : int 
     
     def __init__(self) :
         super().__init__(energie_initiale = 3, energie_maximale = 3, age_maximal = float("inf"))
-            #float "inf" (Chatgpt) - les plantes ne meurent pas donc leur âge maximal est infini
+            #float "inf" (Chatgpt) - les plantes ne meurent pas donc leur âge maximal est infini, ou sinon ont met l'attribut age maximal que dans animal ?
         self.valeur_nutritive = 3
-    
     
     def se_faire_manger(self) :
         self.vivant = False 
-        #Est-ce qu'on ajoute le fait de fournir de l'énergie ?
+        return self.valeur_nutritive
         
-        
+   
 class Animal(EtreVivant) :
     reproduction : Tuple[int,int]
+    cout_reproduction : int
     vitesse : int
     
-    def __init__(self, energie_initiale : int, energie_maximale : int, age_maximal : int, reproduction : Tuple[int, int]) :
+    def __init__(self,energie_initiale : int, energie_maximale : int, age_maximal : int, reproduction : Tuple[int, int], cout_reproduction : int) :
         super().__init__(energie_initiale, energie_maximale, age_maximal)
         self.reproduction = reproduction
+        self.cout_reproduction = cout_reproduction
             
-    #def se_reproduire(self) -> int : 
-        #A définir : nombre aleatoire allant d'un int à un int, définit dans "reproduction"
+    def se_reproduire(self ) -> List["Animal"] : 
+        if not self.vivant :
+            return []       #Code peut être inutile car dans tous les cas pour rencontrer un animal, il doit être vivant
+        if self.energie < self.cout_reproduction:
+            return []                               #Doit avoir l'énergie necessaire pour se reporduire
+        self.energie -= self.cout_reproduction      #Devra être ajuster pour être proportionnel au nombre d'enfants si demandé
+        nombre_enfants = random.randint(self.reproduction[0], self.reproduction[1]) 
         
-    def se_deplacer(self, environnement):
-        #A définir plus tard
+        enfants = []
+        for _ in range(nombre_enfants) : #ChatGPT : underscore for _ ... convention lorsque la variable n'a pas besoin d'être utilisée dans la boucle
+            enfant = type(self)( #chatGPT : type(self) nous renvoi la classe de l'objet self qui ici est la classe Lapin car on est dans sa définition
+                energie_initiale=self.energie, 
+                energie_maximale=self.energie_maximale,
+                vitesse=self.vitesse,
+                age_maximal=self.age_maximal,
+                portee_reproduction=self.portee_reproduction,
+                cout_reproduction=self.cout_reproduction
+            )
+            enfants.append(enfant) #On mettra plutôt environnement.append(enfant) quand l'environnement sera créer 
+            return enfants
 
 
-#Class Lapin paramètres initiaux:
-lapin_nombre_initial = 520
-lapin_energie_initiale = 10
-lapin_energie_maximale = 20
-lapin_vitesse = 1
-lapin_age_maximal = 5
-lapin_reproduction_max = 3
-lapin_reproduction_min = 1
-lapin_energie_fournie = 16
+    #def se_deplacer(self, environnement):
+        
+        
+
 
 class Lapin(Animal):
     valeur_nutritive : int
     def __init__(self):
-        super().__init__(energie_initiale=10, energie_maximale=20, vitesse=1, age_maximal=5, reproduction=(1, 3))
+        super().__init__(energie_initiale=10, energie_maximale=20, vitesse=1, age_maximal=5, reproduction=(1, 3), cout_reproduction= 2)
         self.valeur_nutritive = 16
+    
+    def se_faire_manger(self) :
+        self.vivant = False 
+        return self.valeur_nutritive
+            
         
-        
-#Class Renard, paramètres initiaux :
-renard_nombre_initial = 22
-renard_energie_initiale = 25
-renard_energie_maximale = 50
-renard_vitesse = 2
-renard_age_maximal = 3
-renard_reproduction_max = 5
-renard_reproduction_min = 1
 
 class Renard(Animal) :
     def __init__(self):
-        super().__init__(energie_initiale=25, energie_maximale= 50, vitesse = 2, age_maximal=3, reproduction = (1,5))
+        super().__init__(energie_initiale=25, energie_maximale= 50, vitesse = 2, age_maximal=3, reproduction = (1,5), cout_reproduction= 4)
         
 #comment on fait pour optenir un diagramme déja ?
 
